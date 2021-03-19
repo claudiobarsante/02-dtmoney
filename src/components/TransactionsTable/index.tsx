c14553e43e8d4111d3c6e1a1a81d02a29513b700
Claudio Barsante <clbmribas@gmail.com>
2021-03-19 16:03:27 -0300

    configuring Miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container } from './styles';
 
 const TransactionsTable = () => {
+	useEffect(() => {
+		fetch('http://localhost:3000/api/transactions')
+			.then(response => response.json())
+			.then(data => console.log(data));
+	});
 	return (
 		<Container>
 			<table>
 				<thead>
-					<th>Description</th>
-					<th>Price</th>
-					<th>Category</th>
-					<th>Date</th>
+					<tr>
+						<th>Description</th>
+						<th>Price</th>
+						<th>Category</th>
+						<th>Date</th>
+					</tr>
 				</thead>
 				<tbody>
 					<tr>
@@ -19,7 +26,7 @@ const TransactionsTable = () => {
 						<td>13/04/2021</td>
 					</tr>
 					<tr>
-						<td>Desenvolvimento de site</td>
+						<td>Desenvolvimento de siteyanr</td>
 						<td className='outcome'>-R$ 12000</td>
 						<td>Venda</td>
 						<td>13/04/2021</td>
